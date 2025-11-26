8ec90a8130c64cfedfe279829a7c082deb294072
Wiktor Chumakov <Apostal333@gmail.com>
2025-11-26 22:10:46 +0300

    Modified Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,22 +13,34 @@ fun main() {
         consumerA.lockFirstAndTrySecond(resourceA, resourceB)
     }
     val t2 = thread {
-        consumerB.lockFirstAndTrySecond(resourceB, resourceA)
+        consumerB.lockFirstAndTrySecond(resourceA, resourceB)
+    }
+    val t3 = thread {
+        Consumer("D").lockFirstAndTrySecond(resourceA,resourceB)
     }
 
     t1.join()
     t2.join()
+    t3.join()
 
     println("main successfully finished")
 }
-
+    /* Избавляемся от Deadlock захватываем ресурсы по хешу
+    Золотое правило: Всегда захватывать общие ресурсы
+    в одном и том же порядке (например, по хэшу, имени, идентификатору)
+     */
 class Consumer(private val name: String) {
     fun lockFirstAndTrySecond(first: Any, second: Any) {
-        synchronized(first) {
+        val (low,high) = if (System.identityHashCode(first) < System.identityHashCode(second)) {
+            first to second
+        }else {
+            second to first
+        }
+        synchronized(low) {
             println("$name locked first, sleep and wait for second")
             Thread.sleep(1000)
+            lockSecond(high)
         }
-        lockSecond(second)
     }
 
     fun lockSecond(second: Any) {
